53e2e1f03106ac961074c85541106210953bcf46
Roman Smetana <smetarom@gmail.com>
2015-11-18 12:10:25 +0100

    Add validation on quest challenge hashtag

--- myicpc-service/src/main/java/com/myicpc/service/validation/QuestChallengeValidator.java
+++ myicpc-service/src/main/java/com/myicpc/service/validation/QuestChallengeValidator.java
@@ -3,6 +3,7 @@ package com.myicpc.service.validation;
 import com.myicpc.model.quest.QuestChallenge;
 import com.myicpc.repository.quest.QuestChallengeRepository;
 import com.myicpc.service.exception.BusinessValidationException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class QuestChallengeValidator extends BusinessEntityValidator<QuestChallenge> {
+    private static final String HASHTAG_REGEXP = "[a-zA-Z0-9]*";
 
     @Autowired
     private QuestChallengeRepository challengeRepository;
@@ -24,5 +26,10 @@ public class QuestChallengeValidator extends BusinessEntityValidator<QuestChalle
         if (duplicated != null && !duplicated.getId().equals(challenge.getId())) {
             throw new BusinessValidationException("quest.error.duplicatedHashtag");
         }
+
+        if (StringUtils.isNotEmpty(challenge.getHashtagSuffix()) && !challenge.getHashtagSuffix().matches(HASHTAG_REGEXP)) {
+            throw new BusinessValidationException("quest.error.invalidHashtag");
+        }
+
     }
 }
